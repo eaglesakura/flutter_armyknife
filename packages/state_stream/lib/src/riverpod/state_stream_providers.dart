f1d62478dc085ac7e3a8dfa7dc7d4750232b3c4a
eaglesakura <eagle.sakura@gmail.com>
2025-02-15 21:03:40 +0900

    add: state_stream/riverpodを追加

---
+++ packages/state_stream/lib/src/riverpod/state_stream_providers.dart
@@ -0,0 +1,34 @@
+import 'package:riverpodx/riverpodx.dart';
+import 'package:state_stream/src/state_stream.dart';
+
+final class StateStreamProviders {
+  /// Provider.autoDispose()を使ってStateを取得するProviderを生成する.
+  static const autoDispose = _AutoDisposeProviders._();
+
+  const StateStreamProviders._();
+}
+
+final class _AutoDisposeProviders {
+  const _AutoDisposeProviders._();
+
+  /// [StateStream] からStateを取得するProviderを生成する.
+  /// StateStream自体の生成は別な [stateStreamProvider] に任されている.
+  AutoDisposeProvider<T> state<T>(
+    AutoDisposeProvider<StateStream<T>> stateStreamProvider,
+  ) {
+    return Provider.autoDispose<T>(
+      (ref) {
+        final stateStream = ref.watch(stateStreamProvider);
+
+        final subscribe = stateStream.stream.listen((state) {
+          ref.state = state;
+        });
+        ref.onDispose(subscribe.cancel);
+        return stateStream.state;
+      },
+      dependencies: [
+        stateStreamProvider,
+      ],
+    );
+  }
+}
--- packages/state_stream/lib/state_stream.dart
+++ packages/state_stream/lib/state_stream.dart
@@ -3,4 +3,5 @@ library;
 export 'src/dispatcher.dart';
 export 'src/mutable_state_stream.dart';
 export 'src/mutable_state_stream_emitter.dart';
+export 'src/riverpod/state_stream_providers.dart';
 export 'src/state_stream.dart';
