79bd96d5e0f787523092f563f9f2f3f08b777880
eaglesakura <eagle.sakura@gmail.com>
2025-04-12 21:30:45 +0900

    add: Rivepod対応用のListプロパティを追加

---
+++ packages/riverpodx/lib/src/list_select_property.dart
@@ -0,0 +1,30 @@
+import 'package:collection/collection.dart';
+
+/// List型のRiverpodプロパティ.
+/// RiverpodとListを組み合わせた場合、論理一致を判定することができないため、
+/// 一致を判定するためのプロパティを作成する.
+class ListSelectProperty<T> {
+  static final _listEquals = ListEquality();
+
+  final List<T>? list;
+
+  @override
+  late final int hashCode = _listEquals.hash(list);
+
+  ListSelectProperty({
+    required this.list,
+  });
+
+  @override
+  bool operator ==(Object other) {
+    if (other is! ListSelectProperty<T>) {
+      return false;
+    }
+    return _listEquals.equals(list, other.list);
+  }
+
+  @override
+  String toString() {
+    return list.toString();
+  }
+}
