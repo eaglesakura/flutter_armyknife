e98f7da957abe086ad2fe3c3c96a0e91bf56ff22
eaglesakura <eagle.sakura@gmail.com>
2025-04-14 18:36:35 +0900

    add: 起点時間を示す定数を追加

--- packages/datex/lib/src/instantx.dart
+++ packages/datex/lib/src/instantx.dart
@@ -1,6 +1,9 @@
 import 'package:time_machine/time_machine.dart';
 
 final class InstantX {
+  /// Unixエポック（1970年1月1日 00:00:00 UTC）を表す時刻.
+  static final epoch = Instant.fromEpochMilliseconds(0);
+
   const InstantX._();
 
   /// 現在のミリ秒単位の時刻を取得する.
