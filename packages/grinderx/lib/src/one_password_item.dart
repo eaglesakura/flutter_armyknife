b6b7b4f32a13f638356d406bb278fd642e53eb92
eaglesakura <eagle.sakura@gmail.com>
2025-06-04 10:14:36 +0900

    add: op readでファイル保存に対応

--- packages/grinderx/lib/src/one_password_item.dart
+++ packages/grinderx/lib/src/one_password_item.dart
@@ -1,7 +1,11 @@
+import 'dart:io';
+
 import 'package:grinder/grinder.dart';
 
 /// One Passwordに保存されてている１アイテム用のアクセサオブジェクト.
 /// アクセスの実態は `op` コマンドである.
+///
+/// `op signin` コマンドを使用し、事前に1passwordの任意アカウントにログインしておく必要がある.
 class OnePasswordItem {
   final String vault;
   final String item;
@@ -26,5 +30,25 @@ class OnePasswordItem {
     } else {
       return result;
     }
+  }
+
+  /// 指定した1passwordアイテムをファイルに保存する.
+  /// 内部実装は `op` コマンドを使用するため、事前にインストールとログインが必要.
+  ///
+  /// [force] がtrueの場合、ファイルが存在する場合は上書きされる.
+  /// デフォルトはtrueである.
+  Future<String> readFile({
+    required File output,
+    bool force = true,
+    required String field,
+  }) async {
+    final result = await runAsync('op', arguments: [
+      'read',
+      if (force) '--force',
+      '--out-file',
+      output.path,
+      'op://$vault/$item/$field',
+    ]);
+    return result;
   }
 }
